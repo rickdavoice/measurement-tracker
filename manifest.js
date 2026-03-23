cba557177077803ca205754a092cd7f24e55fc7c
rickdavoice <rickdavoice@gmail.com>
2026-03-23 18:17:01 -0400

    f

--- manifest.js
+++ manifest.js
@@ -7,13 +7,8 @@
   "theme_color": "#6c3483",
   "icons": [
     {
-      "src": "icon-192.png",
+     "src": "icon.png",
       "sizes": "192x192",
-      "type": "image/png"
-    },
-    {
-      "src": "icon-512.png",
-      "sizes": "512x512",
       "type": "image/png"
     }
   ]
